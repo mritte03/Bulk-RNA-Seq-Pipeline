3bb7b4a489f6d20a6e59490342aa1aed9cf3377e
mritte03 <madison.ritter@tufts.edu>
2025-12-08 14:08:40 -0500

    added sanity checks

--- HTseq-Count/submit_htseq_jobs.sh
+++ HTseq-Count/submit_htseq_jobs.sh
@@ -6,11 +6,26 @@
 # USAGE: bash submit_htseq_jobs.sh get_htseq_counts_single.sh /path/to/annotation.gtf
 # NOTE: be sure to update get_htseq_counts_single.sh with appropriate flags before running!!
 
-BASE_DIR="$(pwd)"
 GET_HTSEQ_COUNTS_SINGLE_FILE=$1
 GTF_FILE=$2
 
-for samfile in "$BASE_DIR"/*.sorted.sam
+### sanity checks ###
+shopt -s nullglob
+files=(*.sorted.sam)
+
+if [ ${#files[@]} -eq 0 ]; then
+    echo "ERROR: No *.sorted.sam files found in current directory."
+    echo "NOTE: Make sure to work from the directory of your SAM files."
+    exit 1
+fi
+
+echo "Found ${#files[@]} sorted SAM files."
+for f in "${files[@]}"; do
+    echo "  - $f"
+done
+
+### submit jobs ###
+for samfile in *.sorted.sam
 do
     sbatch "$GET_HTSEQ_COUNTS_SINGLE_FILE" "$samfile" "$GTF_FILE"
 done
