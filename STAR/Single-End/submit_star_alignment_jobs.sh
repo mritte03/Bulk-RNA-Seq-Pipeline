e35f6a6a65eea56013d51d04c795920364f63482
mritte03 <madison.ritter@tufts.edu>
2025-12-11 17:41:03 -0500

    Update and rename STAR/submit_star_alignment_jobs.sh to STAR/Single-End/submit_star_alignment_jobs.sh

--- STAR/Single-End/submit_star_alignment_jobs.sh
+++ STAR/Single-End/submit_star_alignment_jobs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Runs STAR alignment for all FASTQ files in the current directory
+# Runs STAR alignment for all single-end FASTQ files in the current directory
 # Madison Ritter, 12-11-2025
 
-# DEPENDENCY: run_star_alignment_PAIRED.sh   or   run_star_alignment_SINGLE.sh
+# DEPENDENCY: run_star_alignment_SINGLE.sh
 # USAGE: sbatch submit_star_alignment_jobs.sh run_star_alignment_*.sh GENOME_INDEX
 # NOTE: be sure to work in the directory containing the FASTQ files
 
