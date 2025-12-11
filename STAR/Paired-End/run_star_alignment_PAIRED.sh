9b7cc690509161e954f75e438d2d8a36cbcd082d
mritte03 <madison.ritter@tufts.edu>
2025-12-11 18:16:10 -0500

    Update run_star_alignment_PAIRED.sh

--- STAR/Paired-End/run_star_alignment_PAIRED.sh
+++ STAR/Paired-End/run_star_alignment_PAIRED.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Takes a refernece genome index and * PAIRED-END * FASTQ file to perform STAR alignment
-# Madison Ritter, 12-8-2025
+# Takes a reference genome index and * PAIRED-END * FASTQ file to perform STAR alignment
+# Madison Ritter, 12-11-2025
 
-# USAGE: sbatch run_star_alignment_PAIRED.sh GENOME_INDEX FILE.fastq
-# NOTE: can be used with submit_star_alignment_jobs.sh to run EACH FASTQ as a separate slurm job
+# USAGE: sbatch run_star_alignment_PAIRED.sh GENOME_INDEX READ_1.fqz READ_2.fq.gz
+# NOTE: use submit_star_alignment_jobs_PAIRED.sh to run EACH FASTQ read pair as a separate slurm job
 # NOTE: be sure to work in the directory containing the FASTQ files
 
 # NOTE: you may need to modify the filename patterns below based on how your FASTQ files are named
@@ -12,37 +12,34 @@
 #SBATCH --mem=64G
 #SBATCH -t 4:00:00
 #SBATCH --job-name=star_paired_end
-#SBATCH -o Logs/star_%x_%A.out
-#SBATCH -e Logs/star_%x_%A.err
+#SBATCH -o Logs/%x_%A.out
+#SBATCH -e Logs/%x_%A.err
 
-GENOME_INDEX="$1"   # path to STAR genome index
+GENOME_INDEX="$1"
+R1="$2"
+R2="$3"
 
 module load star/2.7.11b || { echo "ERROR: STAR failed to load" >&2; exit 1; }
 echo "star/2.7.11b loaded successfully."
 
 mkdir -p SAMs
 
-for r1 in *_R1_001.fastq.gz
-do
-    r2="${r1/_R1_/_R2_}"
+filename=$(basename "$R1" .fastq.gz)
 
-    filename=$(basename "$r1" _R1_001.fastq.gz)
+echo "Aligning $filename (paired-end)..."
 
-    echo "Aligning $filename (paired-end)..."
-
-    STAR \
-        --genomeDir "$GENOME_INDEX" \
-        --runThreadN 16 \
-        --readFilesIn "$r1" "$r2" \
-        --readFilesCommand zcat \
-        --outFileNamePrefix "SAMs/${filename}." \
-        --outSAMtype SAM \
-        --outFilterType BySJout \
-        --outFilterMultimapNmax 20 \
-        --alignSJoverhangMin 8 \
-        --alignSJDBoverhangMin 1 \
-        --outFilterMismatchNmax 999 \
-        --outFilterMismatchNoverReadLmax 0.04 \
-        --alignIntronMin 20 \
-        --alignIntronMax 1000000
-done
+STAR \
+    --genomeDir "$GENOME_INDEX" \
+    --runThreadN 16 \
+    --readFilesIn "$R1" "$R2" \
+    --readFilesCommand zcat \
+    --outFileNamePrefix "SAMs/${filename}." \
+    --outSAMtype SAM \
+    --outFilterType BySJout \
+    --outFilterMultimapNmax 20 \
+    --alignSJoverhangMin 8 \
+    --alignSJDBoverhangMin 1 \
+    --outFilterMismatchNmax 999 \
+    --outFilterMismatchNoverReadLmax 0.04 \
+    --alignIntronMin 20 \
+    --alignIntronMax 1000000
